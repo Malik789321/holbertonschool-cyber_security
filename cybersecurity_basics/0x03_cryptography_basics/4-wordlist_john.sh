9feeb9b7672ba34dbf2607417c6af9eeef2580d8
Malik789321 <13734@holbertonstudents.com>
2026-05-31 12:53:44 +0000

    fix wordlist john script

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john "$1" --wordlist=/usr/share/wordlists/rockyou.txt
-john --show "$1" | grep -v ":" > 4-password.txt
+john "$1" --wordlist=/usr/share/wordlists/rockyou.txt && john --show "$1" | grep -v ":" > 4-password.txt
